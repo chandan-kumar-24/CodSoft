f0e59382f28113885a794fd50d8f61f620cd49d9
Chandan Kumar Sahu <161872052+chandan-kumar-24@users.noreply.github.com>
2024-06-07 01:13:57 +0530

    Task 2

---
+++ task2.java
@@ -0,0 +1,50 @@
+  // STUDENT GRADE CALCULATOR
+
+import java.util.Scanner;
+public class task2{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number of subjectes: ");
+		int numSubjects=sc.nextInt();
+		if(numSubjects <=0) {
+			System.out.println("Please enter a valid number of the subject. ");
+			return;
+		}
+		int totalMarks=0;
+		int maxMarksPerSubject=100;
+		for(int i=1; i<=numSubjects;i++) {
+			System.out.println("Enter marks obtained in subject "+i+ " (out of 100):");
+			int marks=sc.nextInt();
+			if(marks<0 || marks > maxMarksPerSubject) {
+				System.out.println("Marks should be in the range of 0 to 100. please enter valid marks. ");
+				i--;
+			}
+			else {
+				totalMarks+=marks;
+			}
+		}
+		double averagePercentage =(double) totalMarks / (numSubjects * maxMarksPerSubject)*100;
+		System.out.println("Total marks: " +totalMarks);
+		System.out.println("average Percentage: "+ averagePercentage + "%");
+		String grade;
+		if(averagePercentage >=90) {
+			grade = "A+";
+		}
+		else if(averagePercentage >=80) {
+			grade = "A";
+	}
+		else if(averagePercentage >=70) {
+			grade = "B";
+		}
+		else if(averagePercentage >=60) {
+			grade = "C";
+		}
+		else if(averagePercentage >=50) {
+			grade = "D";
+		}
+		else {
+			grade = "F";
+		}
+		System.out.println("Grade: "+grade);
+}
+}
